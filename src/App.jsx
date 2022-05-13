29f14eef74206ae7aa57d8c2457fd4de1b007b3f
Juan DanieL Pedro Palacios <isc18350330@gmail.com>
2022-05-13 10:26:02 -0500

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-
-
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
-}
+  const persona = {
+    nombre: "Juan Daniel",
+    apellidoPaterno: "Pedro",
+    apellidoMaterno: "Palacios",
+    edad: 24,
 
+  };
+
+  const nuevoTelefono = "556 887 2222"
+
+  const { nombre, apellidoPaterno, apellidoMaterno, edad} = persona;
+  
+  const contactoPersona = {
+    telefono : "2871202880",
+    email : "dan@gmail.com",
+    empresa : "ITTUX",
+  }
+
+  const infoPersona = { ...persona, ...contactoPersona, puesto: "Comerciante", nuevoTelefono };
+
+  /*
+  console.log (persona);
+  console.log (contactoPersona);
+  console.log (infoPersona);
+*/
+  const frutas = ["manzana", "naranja", "fresa", "pera"];
+  const vegetales = ["lechuga", "zanahoria", "alcachofa"];
+  const carnes = ["res", "cerdo", "pollo", "pescado"];
+
+  const alimentos =[
+    ...frutas,
+    ...vegetales,
+    ...carnes,
+    "Cualquier otro alimento"
+  ];
+
+  console.table (frutas);
+  console.table (vegetales);
+  console.table (carnes);
+  console.table (alimentos);
+  
+  return (
+  <div className = "App">
+    <h3>
+      {`Soy ${nombre} ${apellidoPaterno} ${apellidoMaterno}
+       y tengo ${edad} años.`}
+     </h3>
+  
+  </div>);
+}
 export default App;
